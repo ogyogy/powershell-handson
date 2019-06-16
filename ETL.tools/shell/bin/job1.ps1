f8cc99b013f3c8df7c138f693b6b6e056cf58f0a
ogyogy <47146269+ogyogy@users.noreply.github.com>
2019-06-16 18:15:19 +0900

    ファイルの移動処理を追加

--- ETL.tools/shell/bin/job1.ps1
+++ ETL.tools/shell/bin/job1.ps1
@@ -13,11 +13,41 @@ if (!(Test-Path $uploadPath)) {
 }
 
 $fileList = Get-ChildItem $uploadPath/* -include *.csv | Sort-Object LastWriteTime
-foreach ($file in $fileList) {
-    $tmpPath = Join-Path $scriptPath "/../../csv/tmp"
-    Copy-Item $file ((Join-Path $tmpPath ($file.BaseName + "_tmp.csv")))
-    $utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
-    # remove duplicate lines and overwrite
-    [System.IO.File]::WriteAllLines($file, (Get-Content $file | Sort-Object | Get-Unique), $utf8NoBomEncoding)
+foreach ($uploadCsv in $fileList) {
+    $uploadFile = $uploadCsv.BaseName + $uploadCsv.Extension
 
+    $tmpPath = Join-Path $scriptPath "/../../csv/tmp"
+    $tmpCsv = ((Join-Path $tmpPath ($uploadCsv.BaseName + "_tmp.csv")))
+
+    $rejectPath = Join-Path $scriptPath "/../../csv/reject"
+    $rejectCsv = ((Join-Path $rejectPath $uploadFile))
+
+    $backupPath = Join-Path $scriptPath "/../../csv/backup"
+    $backupCsv = ((Join-Path $backupPath $uploadFile))
+
+    Copy-Item $uploadCsv $tmpCsv
+
+    try {
+        $utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+        # remove duplicate lines and overwrite
+        [System.IO.File]::WriteAllLines($uploadCsv, (Get-Content $uploadCsv | Sort-Object | Get-Unique), $utf8NoBomEncoding)
+        
+        # TODO: upload to azure
+    
+        # TODO: if upload success
+        Move-Item $uploadCsv $backupCsv -force
+        Remove-Item $tmpCsv
+
+        # DEBUG: throw error
+        # throw "err"
+    }
+    catch {
+        # TODO: if process failure
+        # Read-Host "Copy tmp csv to upload csv"
+        Copy-Item $tmpCsv $uploadCsv
+        # Read-Host "Copy tmp csv to reject csv"
+        Copy-Item $tmpCsv $rejectCsv
+        # Read-Host "Remove tmp csv"
+        Remove-Item $tmpCsv
+    }
 }
